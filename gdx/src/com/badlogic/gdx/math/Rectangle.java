3434f9a963e6dc3d48b8071b09b45d3885cd5460
HoyongJeong <hyjung888@naver.com>
2017-05-08 17:41:09 +0900

    1. Inline Temp 2. contain(Circle) in rectangle class 3. easily understand

--- gdx/src/com/badlogic/gdx/math/Rectangle.java
+++ gdx/src/com/badlogic/gdx/math/Rectangle.java
@@ -198,8 +198,9 @@ public class Rectangle implements Serializable, Shape2D {
 	/** @param circle the circle
 	 * @return whether the circle is contained in the rectangle */
 	public boolean contains (Circle circle) {
-		return (circle.x - circle.radius >= x) && (circle.x + circle.radius <= x + width)
-			&& (circle.y - circle.radius >= y) && (circle.y + circle.radius <= y + height);
+		return contains(circle.x, circle.y);
+
+
 	}
 
 	/** @param rectangle the other {@link Rectangle}.
