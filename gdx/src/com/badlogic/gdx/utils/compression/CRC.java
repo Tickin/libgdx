b1d648ccad3b5c579f966bd04d20c484cdb10533
lunar456th <lunar456th@naver.com>
2017-05-08 13:25:15 +0900

    Refactoring Operation: Substitute magic number to symbolic constant
    Refactoring target class: utils.compression::CRC class
    The reason of applying refactoring operation
     : To specify more clearly what the constant numbers(256, 8) mean.

--- gdx/src/com/badlogic/gdx/utils/compression/CRC.java
+++ gdx/src/com/badlogic/gdx/utils/compression/CRC.java
@@ -3,12 +3,15 @@
 package com.badlogic.gdx.utils.compression;
 
 public class CRC {
-	static public int[] Table = new int[256];
+	final static int TABLE_LENGTH = 256;
+	final static int BYTE = 8;
+
+	static public int[] Table = new int[TABLE_LENGTH];
 
 	static {
-		for (int i = 0; i < 256; i++) {
+		for (int i = 0; i < TABLE_LENGTH; i++) {
 			int r = i;
-			for (int j = 0; j < 8; j++)
+			for (int j = 0; j < BYTE; j++)
 				if ((r & 1) != 0)
 					r = (r >>> 1) ^ 0xEDB88320;
 				else
@@ -25,17 +28,17 @@ public class CRC {
 
 	public void Update (byte[] data, int offset, int size) {
 		for (int i = 0; i < size; i++)
-			_value = Table[(_value ^ data[offset + i]) & 0xFF] ^ (_value >>> 8);
+			_value = Table[(_value ^ data[offset + i]) & 0xFF] ^ (_value >>> BYTE);
 	}
 
 	public void Update (byte[] data) {
 		int size = data.length;
 		for (int i = 0; i < size; i++)
-			_value = Table[(_value ^ data[i]) & 0xFF] ^ (_value >>> 8);
+			_value = Table[(_value ^ data[i]) & 0xFF] ^ (_value >>> BYTE);
 	}
 
 	public void UpdateByte (int b) {
-		_value = Table[(_value ^ b) & 0xFF] ^ (_value >>> 8);
+		_value = Table[(_value ^ b) & 0xFF] ^ (_value >>> BYTE);
 	}
 
 	public int GetDigest () {
