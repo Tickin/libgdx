0c3baf1af1d37121a22897a41d215feb62d42000
HoyongJeong <hyjung888@naver.com>
2017-05-08 17:49:26 +0900

    1. Inline Temp 2. merge(Rectangle) in Rectangle 3. easy understand

--- gdx/src/com/badlogic/gdx/math/Rectangle.java
+++ gdx/src/com/badlogic/gdx/math/Rectangle.java
@@ -237,16 +237,9 @@ public class Rectangle implements Serializable, Shape2D {
 	 * @param rect the other rectangle
 	 * @return this rectangle for chaining */
 	public Rectangle merge (Rectangle rect) {
-		float minX = Math.min(x, rect.x);
-		float maxX = Math.max(x + width, rect.x + rect.width);
-		x = minX;
-		width = maxX - minX;
-
-		float minY = Math.min(y, rect.y);
-		float maxY = Math.max(y + height, rect.y + rect.height);
-		y = minY;
-		height = maxY - minY;
-
+		merge(rect.x,rect.y);
+		merge(rect.x+rect.width,rect.y+rect.height);
+		
 		return this;
 	}
 
